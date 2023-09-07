68868c0172de14eb5388cc0a6250d4a8d152b7f0
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-09-07 19:35:35 +0100

    setting up the app.js file

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+
+const connectDB = require('./db/connect');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("ecommerce app");
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+port = process.env.PORT || 5000;
+const starter = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log("Database connected!!!");
+        app.listen(port, () => {
+            console.log(`server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error.message);
+    };
+};
+
+starter();

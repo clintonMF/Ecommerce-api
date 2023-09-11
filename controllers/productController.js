5c2b32061e1e4d56b6746f41f59391cfab55c64f
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-09-11 23:04:43 +0100

    update product and delete product functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,25 +5,41 @@ const errors = require('../errors');
 
 const createProduct = async (req, res) => {
     req.body.user = req.user.userID;
+    console.log(req.body.user);
     const product = await Product.create(req.body);
     res.status(StatusCodes.CREATED).json(product);
 };
+
 const getAllProducts = async(req, res) => {
     const products = await Product.find({});
     res.status(StatusCodes.OK).json({products});
 };
+
 const getSingleProduct = async(req, res) => {
     const product = await Product.findOne({_id: req.params.id});
     if (!product) {
-        throw new errors.NotFoundError(`product with id ${req.params.id}`);
+        throw new errors.NotFoundError(`No product with id ${req.params.id}`);
     };
     res.status(StatusCodes.OK).json({product});
 };
+
 const updateProduct = async(req, res) => {
-    res.send('updateProduct')
+    const product = await Product.findOneAndUpdate(
+        {_id: req.params.id}, 
+        req.body, 
+        {new: true, runValidators: true}
+    );
+    if (!product) {
+        throw new errors.NotFoundError(`No product with id ${req.params.id}`);
+    };
+    res.status(StatusCodes.OK).json({product});
 };
 const deleteProduct = async(req, res) => {
-    res.send('deleteProduct')
+    const product = await Product.findOneAndDelete({_id: req.params.id});
+    if (!product) {
+        throw new errors.NotFoundError(`No product with id ${req.params.id}`);
+    };
+    res.status(StatusCodes.OK).json({product});
 };
 const uploadImage = async(req, res) => {
     res.send('uploadImage')
